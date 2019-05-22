427a0ff52879071c2325260bf20ae6b9bb5b2382
Cherise <cherisetan@live.com>
2019-05-22 13:18:31 +1200

    Edit comments

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 // jshint esversion: 6
 
-// require the following packages
+// Require the following packages
 const express = require("express");
 const hbs = require("express-handlebars");
 const flash = require("connect-flash");
 const session = require("express-session");
 const passport = require("passport");
 
-// import the routes folders, and save them as variables
+// Import and save the routes folders
 const routes = require("./routes/routes");
 const usersRoutes = require("./routes/users");
 const destinationRoutes = require("./routes/destinations");
 const activitiesRoutes = require("./routes/activities");
 const accommodationRoutes = require("./routes/accommodation");
 
-// save express() as a variable for ongoing use
+// Save express() as a variable for ongoing use
 const app = express();
 
-// require passport config
+// Require passport config
 require("./config/passport")(passport);
 
-// middleware setup for handlebars (copied from Leslie's project)
+// Middleware setup for handlebars
 app.set('views', './views');
 app.engine("hbs", hbs({defaultLayout: "dashboard", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-// set up for express
+// Set up for express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// set up for express session
+// Set up for express session
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true
 }));
 
-// set up passport middleware
+// Set up passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// set up for connect flash
+// Set up for connect flash
 app.use(flash());
 
-// global variables for flash (allows colours)
+// Global variables for flash (allows colours)
 app.use((req, res, next) => {
   res.locals.successMsg = req.flash("successMsg");
   res.locals.errorMsg = req.flash("errorMsg");
@@ -54,12 +54,12 @@ app.use((req, res, next) => {
 // Pull in ensureAuthenticated to protect routes
 const {ensureAuthenticated} = require("./config/auth");
 
-// set up routes
+// Set up routes
 app.use("/", routes);
 app.use("/users", usersRoutes);
 app.use("/destinations", ensureAuthenticated, destinationRoutes);
 app.use("/accommodation", ensureAuthenticated, accommodationRoutes);
 app.use("/activities", ensureAuthenticated, activitiesRoutes);
 
-// export 'app' for use elsewhere (particulalry index.js file)
+// Export 'app' for use elsewhere (particulalry index.js file)
 module.exports = app;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // jshint esversion:6
 
-// this is the main file which will by run by 'nodemon'
-const app = require("./app"); // this requires the 'app.js' file
+// This is the main file which will by run by 'nodemon'
+const app = require("./app"); // This requires the 'app.js' file
 
-const port = process.env.PORT || 3000; // server will run on port 3000, unless it is deployed
+const port = process.env.PORT || 3000; // Server will run on port 3000, unless it is deployed
 
-app.listen(port, function(){ // server will listen on the specified port and will log a message when this occurs
+app.listen(port, function(){ // Server will listen on the specified port and will log a message when this occurs
   console.log("Port is running on "+ port);
 });
