9d2fc6922203f16732a998e8771a60090be4e69d
Cherise <cherisetan@live.com>
2019-06-18 11:03:08 +0200

    Update server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+// jshint esversion:6
+
+// Require the 'app.js' file
+const app = require("./app");
+
+// Run the server on port 3000, unless it is deployed
+const port = process.env.PORT || 3000; 
+
+// Server will listen on the specified port and will log a message when this occurs
+app.listen(port, function(){ 
+  console.log("Port is running on " + port);
+});
